7b6e61046ad2e99d1a950b0e2cbdee849841cf67
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-22 09:27:40 +0200

    seeder creation

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +14,21 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 10; $i++) {
+
+            $newProject = new Project;
+
+            $newProject->title = $faker->sentence(3);
+            $newProject->description = $faker->paragraph();
+            $newProject->url = $faker->url();
+            $newProject->status = $faker->word();
+            $newProject->tags = $faker->randomElement(['web app', 'front-end', 'back-end']);
+            $newProject->release_date = $faker->date();
+            $newProject->languages = $faker->randomElement(['html', 'css', 'bootstrap', 'scss', 'javascript', 'vuejs', 'php', 'laravel']);
+
+            $newProject->save();
+        }
     }
 }
